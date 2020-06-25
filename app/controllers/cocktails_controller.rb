f04e9d2810f70ccbf4c0abbef6f65e8eab8fd530
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-25 09:47:42 -0300

    added created functionality

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,4 +2,31 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktails_params)
+    @cocktail.save
+        if @cocktail.save
+          redirect_to cocktails_path(@cocktail)
+        else
+          render :new
+        end
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name, :description)
+  end
+
+
+
 end
