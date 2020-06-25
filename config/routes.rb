a81da5cfba118a9875933e8654f6896203e81617
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-25 11:54:05 -0300

    added new routes for doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :cocktails, only: [:index, :new, :create, :edit, :update]
+    resources :cocktails, only: [:index, :new, :show, :create, :edit, :update] do
+      resources :doses, only: [:new, :create, :destroy]
+    end
   root to: 'cocktails#index'
 end
