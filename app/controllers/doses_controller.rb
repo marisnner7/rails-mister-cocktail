eef15b5f2cc5d88f6c08e9632389d127e44828b2
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-25 13:25:52 -0300

    crud for doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,20 @@
 class DosesController < ApplicationController
   before_action :find_cocktail, only: [:new , :create, :destroy]
 
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+      if @dose.save
+        redirect_to cocktail_path(@cocktail)
+      else
+          render :new
+      end
+
+  end
 
 
   private
@@ -8,4 +22,9 @@ class DosesController < ApplicationController
   def find_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
