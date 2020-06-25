5eac5f93768f9e6f8f2aa29c1b1f6e28e9f2bafb
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-25 09:46:37 -0300

    added new routes for create cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :cocktails, only: [:index, :new, :create, :edit, :update]
   root to: 'cocktails#index'
 end
