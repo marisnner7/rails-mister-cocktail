6d2cc5f28c897542ef783da0d6a909a5d506c6c9
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-25 09:36:25 -0300

    created seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating.."
+Ingredient.create(name: "spider")
+Ingredient.create(name: "Pumpkin Juice")
+Ingredient.create(name: "Fudge Flies")
+
+Cocktail.create(name: "Butterbeer")
+Cocktail.create(name: "Unicorn Blood Cocktail")
+Cocktail.create(name: " Polyjuice Potion")
+Cocktail.create(name: "Witches Brew Cocktail:")
+Cocktail.create(name: " Voldemort Cocktail:")
+
+puts "Done!"
